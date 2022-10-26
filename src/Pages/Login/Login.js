2a72a2c223b7daa3e75cb30ee74877aaa2414c7a
Sharif101 <sharif009009@gmail.com>
2022-10-26 14:28:34 +0600

    login degisn

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,26 +1,23 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import "./Login.css";
 
 const Login = () => {
   return (
     <div>
-      <Form>
+      <Form className="form">
+        <h3>Please Login here </h3>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <button className="log-button">Log In</button>
       </Form>
     </div>
   );
