faa6a2b2fc8819b05afe81fe445f7c81ed8dd50f
Sharif101 <sharif009009@gmail.com>
2022-10-26 10:15:12 +0600

    installs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
