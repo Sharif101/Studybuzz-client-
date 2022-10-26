9955458eafa7a2e76aa0183844dd913804d2b1d4
Sharif101 <sharif009009@gmail.com>
2022-10-26 12:56:35 +0600

    nav added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -3,14 +3,22 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import logo from "../../assest/logo/logo.png";
+import "./Header.css";
 // import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
     <div>
-      <Navbar expand="lg">
+      <Navbar expand="lg" className="nav">
         <Container>
-          <Navbar.Brand>Studypress</Navbar.Brand>
+          <Navbar.Brand>
+            <img className="nav-logo" src={logo} alt="" />
+            <Link to="/" className="nav-title">
+              Study Buzz
+            </Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
@@ -18,6 +26,9 @@ const Header = () => {
               <Link to="/courses">Courses</Link>
               <Link to="/faq">FAQ</Link>
               <Link to="/blog">Blog</Link>
+              <Button className="log-btn" variant="info">
+                Log in
+              </Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
