1eb25c8221298ebde8b0fe2478c44986deb389a9
Sharif101 <sharif009009@gmail.com>
2022-10-27 11:38:37 +0600

    coursedetails added

--- src/Pages/Coursedetails/Coursedetails.js
+++ src/Pages/Coursedetails/Coursedetails.js
@@ -2,8 +2,9 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faCrown } from "@fortawesome/free-solid-svg-icons";
+import "./Coursedetails.css";
 
 const Coursedetails = () => {
   let details = useLoaderData();
@@ -12,17 +13,31 @@ const Coursedetails = () => {
     <div>
       {/* <h1>{details.name}</h1> */}
 
-      <Card>
+      <Card className="card-bd">
         <Card.Img variant="top" src={details.image_url} />
-        <Card.Body>
-          <Card.Title>{details.name}</Card.Title>
-          <Card.Text>{details.details}</Card.Text>
-          <div className="d-flex justify-content-between">
+        <Card.Body className="text">
+          <div>
+            <Card.Title>
+              <h2>{details.name}</h2>
+            </Card.Title>
+            <Card.Text>
+              <p>{details.details}</p>
+            </Card.Text>
+          </div>
+          <div className="card-ic">
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+          </div>
+          <div className="d-flex justify-content-between my-3 card-shortinfo">
             <p>Total Class: {details.total_class}</p>
             <p>Total Project: {details.total_project}</p>
             <p>Total Fee: {details.total_fee}</p>
           </div>
-          <button>Get Prenium</button>
+          <button>
+            Get Prenium <FontAwesomeIcon icon={faCrown}></FontAwesomeIcon>
+          </button>
         </Card.Body>
       </Card>
     </div>
